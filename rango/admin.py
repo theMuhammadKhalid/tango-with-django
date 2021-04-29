d07d9399f1adc444cd609deddcba9515c0ff6779
Muhammad Khalid <themuhammadkhalid@gmail.com>
2021-04-29 11:03:11 +0500

    Models and Databases work done

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from rango.models import Category, Page
+
+class PageAdmin(admin.ModelAdmin):
+    '''Page Admin'''
+    list_display = ('title', 'category', 'url', 'views')
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
