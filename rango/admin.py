0c6d8ddc37ab8f5594447ab02163052ee8adf6b2
Muhammad Khalid <themuhammadkhalid@gmail.com>
2021-05-01 21:38:44 +0500

    MTV work done

--- rango/admin.py
+++ rango/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
 class PageAdmin(admin.ModelAdmin):
     '''Page Admin'''
     list_display = ('title', 'category', 'url', 'views')
 
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -6,12 +6,29 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 
+from rango.models import Category, Page
 
 def index(request):
-    context_dict = {'boldMessage': "Crunchy, creamy, cookie, candy, cupcake !"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, "pages": page_list}
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
     context_dict = {'myName': "Muhammad Khalid"}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+    
+    return render(request, 'rango/category.html', context_dict)
